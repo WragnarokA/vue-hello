7c7cad92338c25fbc789e83f3770dc97eb7d3605
WragnarokA <wilmer.arango.a@gmail.com>
2023-05-08 17:27:34 +0200

    primo msn

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 console.log("ciao");
+
+
+const { createApp } = Vue
+createApp({
+    data() {
+        return{
+            il: "Il",
+            mio: "mio",
+            primo: "primo",
+            messaggio: "messaggio 😁",
+
+        }
+    },
+    methods: {
+
+    } 
+
+}).mount('#app')
+
